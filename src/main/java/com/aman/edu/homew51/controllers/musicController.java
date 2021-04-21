a9746ce97eaa712856d0a5ea91b61b8f7099eb33
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-04-21 18:02:12 +0600

    #5 comiit add controller

--- src/main/java/com/aman/edu/homew51/controllers/musicController.java
+++ src/main/java/com/aman/edu/homew51/controllers/musicController.java
@@ -11,10 +11,10 @@ public class musicController {
     private final musicRepositories repository;
     private final musicService musicService;
 
-//    public musicController(musicRepositories repositories, com.aman.edu.homew51.services.musicService musicService) {
-//        this.repository = repositories;
-//        this.musicService = musicService;
-//    }
+    public musicController(musicRepositories repositories, com.aman.edu.homew51.services.musicService musicService) {
+        this.repository = repositories;
+        this.musicService = musicService;
+    }
 
 
 }
