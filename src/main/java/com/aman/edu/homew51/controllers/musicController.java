a03b127723e0cbe8503ab70f010f06f418464f7b
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-04-21 18:01:18 +0600

    #5 commit

--- src/main/java/com/aman/edu/homew51/controllers/musicController.java
+++ src/main/java/com/aman/edu/homew51/controllers/musicController.java
@@ -1,8 +1,20 @@
 package com.aman.edu.homew51.controllers;
 
+import com.aman.edu.homew51.repositories.musicRepositories;
+import com.aman.edu.homew51.services.musicService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class musicController {
+    private final musicRepositories repository;
+    private final musicService musicService;
+
+//    public musicController(musicRepositories repositories, com.aman.edu.homew51.services.musicService musicService) {
+//        this.repository = repositories;
+//        this.musicService = musicService;
+//    }
+
 
 }
