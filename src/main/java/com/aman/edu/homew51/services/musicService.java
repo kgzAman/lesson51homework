40b9f51042058c9b95f83dc2628061274ba946eb
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-04-21 17:52:44 +0600

    #3 add musicServer constructir

--- src/main/java/com/aman/edu/homew51/services/musicService.java
+++ src/main/java/com/aman/edu/homew51/services/musicService.java
@@ -1,9 +1,16 @@
 package com.aman.edu.homew51.services;
 
 import com.aman.edu.homew51.repositories.musicRepositories;
+import lombok.Data;
 import org.springframework.stereotype.Service;
 
+
+@Data
 @Service
 public class musicService {
     private final musicRepositories musicRepositories;
+
+    public musicService(com.aman.edu.homew51.repositories.musicRepositories musicRepositories) {
+        this.musicRepositories = musicRepositories;
+    }
 }
