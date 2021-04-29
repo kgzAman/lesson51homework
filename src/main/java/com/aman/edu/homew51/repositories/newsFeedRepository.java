3d257e5ce7203f71cb23992e76d21dfe1e147c8a
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-04-29 09:29:37 +0600

    #11 remove old classes make new

--- src/main/java/com/aman/edu/homew51/repositories/newsFeedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.aman.edu.homew51.repositories;
-
-import com.aman.edu.homew51.Entities.newsFeed;
-import org.springframework.data.repository.CrudRepository;
-
-public interface newsFeedRepository extends CrudRepository<newsFeed, String> {
-}
