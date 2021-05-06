f128a03c511626e25c32dc16984412ae8a631ead
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-05-06 14:40:30 +0600

    #16 innitional with Spring Security

--- src/main/java/com/aman/edu/homew51/dto/LoginUserIdDto.java
+++ src/main/java/com/aman/edu/homew51/dto/LoginUserIdDto.java
@@ -2,8 +2,13 @@ package com.aman.edu.homew51.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class LoginUserIdDto {
     private String name;
+    private String email;
+
+    private LocalDateTime localDateTime;
 
 }
