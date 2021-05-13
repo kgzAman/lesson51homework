94c0906b51d2c1593148bba42ed623f2685ddfdc
Aman Kanatbek Uulu <bishkek9408@gmail.com>
2021-05-13 17:33:19 +0600

    add User calss, publication calss, comment class

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,3 +1,24 @@
-let user ={
-    id
+let User ={
+    id:"1",
+    name: "Aman",
+    email: "example@mail.com",
+    password:"JsExample123"
+};
+
+let publication={
+    id:"1",
+    Picture:"/picture.img" ,
+    description :"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    date:"15.12.15",
+    User :User,
+    comment:Comment,
+    // создал обьект user в к класс publication
 }
+
+let comment={
+    id: "1",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+    date:"01.12.16",
+    // добавил обект comment в к класс publication
+}
+
